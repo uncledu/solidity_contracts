26afdc6692eb933ec406b2a1258f384da8eec121
fantasy <timizqi@gmail.com>
2025-07-21 10:30:36 +0800

    refine code

--- 03_ReentryAttack/script/Victim.s.sol
+++ 03_ReentryAttack/script/Victim.s.sol
@@ -6,25 +6,21 @@ import {Victim} from "../src/Victim.sol";
 import {Attacker} from "../src/Attacker.sol";
 
 contract VictimScript is Script {
-  Victim public victim;
+  Victim internal victim;
 
-  Attacker public attacker;
+  Attacker internal attacker;
 
-  function setUp() public {
+  function run() public{
     vm.startBroadcast();
     victim = new Victim();
     attacker = new Attacker(payable(address(victim)));
     vm.deal(address(victim), 10 ether);
     vm.deal(address(attacker), 2 ether);
-    console.logUint(address(victim).balance);
-    console.logUint(address(attacker).balance);
     vm.stopBroadcast();
-  }
-  function run() public{
     vm.startPrank(address(attacker));
     attacker.beginAttack{value: 1 ether}();
     vm.stopPrank();
     require(address(attacker).balance > 2 ether, "should be 11 ether after attack");
-    console.log(address(attacker).balance);
+    // console.log(address(attacker).balance);
   }
 }
